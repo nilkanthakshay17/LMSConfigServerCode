7077cbb450ec00cc70f714501a154b59c2fd5421
nilkanthakshay17 <nilkanthakshay17@gmail.com>
2023-04-24 16:54:31 +0530

    added code to the repo

---
+++ src/test/java/com/cognizant/app/lms/config/server/LmsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.app.lms.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
